922c65fd7b4b15ef1b640ffc03206891ca64b4e5
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2020-11-06 21:01:37 +0200

    Altered calculation algorithms and values

--- Scripts/LogPathLossCalculator.gd
+++ Scripts/LogPathLossCalculator.gd
@@ -6,20 +6,22 @@ const normal = 0
 
 const fsplConst = -27.55 # Free Space Path loss constant when in meters and megahertz
 
-var distance = 10 # In meters
+var distance = 25 # In meters
 var refDistance = 2 # d0
-var frequency = 2.4 * pow(10, 9) # In GHz
+var frequency = 2.4 * 1000 # 2.4 GHz in MHz
 var transmitterGain = -8 # in dB
 var receiverGain = -1.1 # in dB
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var result = calculateLogDistancePathLoss()
+	var result
+	
+	result = calculateLogDistancePathLoss()
 	print("LDPL: " +  str(result) + " dB")
 	result = calculateITU(distance)
 	print("ITU: " +  str(result) + " dB")
-	result = calculateMotleyKeenan([0])
+	result = calculateMotleyKeenan([1])
 	print("MotleyKeenan: " +  str(result) + " dB")
 	pass # Replace with function body.
 
@@ -29,16 +31,16 @@ func _ready():
 #	pass
 
 # Calculates path loss using reference distance
-func calculatePathLoss():
+func calculateFreeSpacePathLoss():
 	var dlog = 20 * (log(refDistance) / log(10))
 	var flog = 20 * (log(frequency) / log(10))
 	var clog = 20 * (log(((4*PI)/c)) / log(10)) 
-	var fspl = dlog + flog + clog - transmitterGain - receiverGain
+	var fspl = dlog + flog + fsplConst
 	return fspl
 
 
 func calculateLogDistancePathLoss():
-	var pl = calculatePathLoss() + 10 * gamma * (log(distance/refDistance) / log(10)) + normal
+	var pl = calculateFreeSpacePathLoss() + 10 * gamma * (log(distance/refDistance) / log(10)) + normal
 	return pl
 
 func calculateMotleyKeenan(obstacles):
@@ -47,11 +49,11 @@ func calculateMotleyKeenan(obstacles):
 	for i in range(0, obstacles.size()):
 		sigma = sigma + obstacles[i]
 	
-	var pl = calculatePathLoss() + 10 * gamma * (log(distance/refDistance) / log(10)) + sigma
+	var pl = calculateFreeSpacePathLoss() + 10 * gamma * (log(distance/refDistance) / log(10)) + sigma
 	return pl
 
 func calculateITU(distanceInMeter):
-	var flog = 20 * (log(frequency * 1000) / log(10)) # Takes in MHz, that's why we multiply frequency by 1000
+	var flog = 20 * (log(frequency) / log(10)) 
 	var nlog = 30 * (log(distanceInMeter)/log(10))
 	# Floors are not important for us so it is exempt
 	var itu = flog + nlog - 28
